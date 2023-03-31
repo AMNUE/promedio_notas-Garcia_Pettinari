c47f798c701ece290444d6669d090280e17a94c3
Manuel Garcia Pettinari <manuel.garciapettinari@gmail.com>
2023-03-30 23:49:54 -0300

    Correccion primera pre entrega

---
+++ js/script.js
@@ -0,0 +1,79 @@
+
+//Inicio del programa
+//let nombreAlumno = prompt("Ingrese el nombre del alumno: ");
+
+//variables numericas-registro de notas
+
+
+localStorage.setItem(`nombre`, nombreAlumno1);
+
+
+
+const nombreAlumno1 = document.getElementById("nombre");
+console.log(nombreAlumno1)
+//nombreAlumno1 = localStorage.getItem(`nombre`);
+
+
+
+
+
+
+
+
+
+/*let nota1 = parseFloat(prompt("Ingrese la primera nota: "));
+for (let i=0; i<1000 ; i++){
+    if (nota1>=0 && nota1<=10){
+        break
+    } else {
+        nota1=parseFloat(prompt("Ingrese un numero valido: "));
+    }
+}
+let nota2 = parseFloat(prompt("Ingrese la segunda nota: "));
+for (let i=0; i<1000 ; i++){
+    if (nota2>=0 && nota2<=10){
+        break
+    } else {
+        nota2 = parseFloat(prompt("Ingrese un numero valido: "));
+    }
+}
+let nota3 = parseFloat(prompt("Ingrese la tercer nota: "));
+for (let i=0; i<1000 ; i++){
+    if (nota3>=0 && nota3<=10){
+        break
+    } else {
+        nota3=parseFloat(prompt("Ingrese un numero valido: "));
+    }
+
+}
+
+
+
+let promedio = (nota1 + nota2 + nota3) /3;
+//calculo del promedio
+function calculoPromedio () {
+    if (promedio<6.5){
+        console.log("El Alumno " + nombreAlumno + ", tuvo un promedio de " + promedio + ", lo que le da la condicion de: Reprobado");
+    } else if (promedio>=6.5){
+        console.log("El Alumno " + nombreAlumno + ", tuvo un promedio de " + promedio + ", lo que le da la condicion de: Aprobado");
+    }
+}
+//calculoPromedio();
+
+const Alumno = {
+    nombre: nombreAlumno,
+    nota: promedio
+}
+
+const arrayAlumnos = [nombreAlumno, promedio];
+
+//for (let i = 0; i<arrayAlumnos.length; i++) {
+    //alert(arrayAlumnos[i])
+    
+//}
+
+
+
+
+//Fin del programa
+*/
